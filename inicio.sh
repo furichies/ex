f2dd85050629aec36639203fcd453bc28c66d984
richifor <134418589+richifor@users.noreply.github.com>
2023-06-14 10:38:29 +0200

    Add files via upload

--- inicio.sh
+++ inicio.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
-vagrant box add ./img_jam.box ubuntu/jam
-git clone https://github.com/richifor/ex
+#A ver como tira en windows
+vagrant box add ubuntu/jam ./img_jam.box 
+echo "agregada la imagen ubuntu/jam"
+sleep 1
+if [ ! -d ./ex ]
+then 
+ git clone https://github.com/richifor/ex
+fi
+echo "entendemos que existe ex y debe estar clonado"
+sleep 1
 cd ex
-echo -e "./id_rsa\n\n"|ssh-keygen -t rsa -b 4096
-chown vagrant:vagrant id_rs*
+
 vagrant up
+echo "entendemos que la cosa ha tirado. Renombramos ficheros para aprovisionar"
+sleep 1
 mv ansiblemachine1.sh ansiblemachine1.sh.old
 mv ansiblemachine2.sh ansiblemachine1.sh
-mv dockermachine1.sh dockermachine1.sh.old
-mv dockermachine2.sh dockermachine1.sh
+echo " a ver si tira el último aprovisionamiento"
+sleep 1
 
 vagrant provision 

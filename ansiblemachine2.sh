08f0caf4aba75a053e4a11c7311487515a0c328d
richifor <134418589+richifor@users.noreply.github.com>
2023-06-14 12:22:43 +0200

    Add files via upload

--- ansiblemachine2.sh
+++ ansiblemachine2.sh
@@ -1,4 +1,4 @@
-echo $USER
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i /home/vagrant/hosts /vagrant/docker-playbook.yaml
 ansible -i /home/vagrant/hosts all -a "usermod -aG docker vagrant" --become
 ansible -i /home/vagrant/hosts all -a "docker compose -f /vagrant/docker-compose.yaml up -d" --become

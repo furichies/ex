feefd1f32e13a9b463a90b218a6c66f2ac2888f8
richifor <134418589+richifor@users.noreply.github.com>
2023-06-14 12:31:45 +0200

    Add files via upload

--- dockermachine2.sh
+++ dockermachine2.sh
@@ -1,4 +1,4 @@
-echo 'FROM alpine\nMAINTAINER res@res.es\nCMD [ "/bin/sh" ]' > Dockerfile
+echo -e 'FROM alpine\nMAINTAINER res@res.es\nCMD [ "/bin/sh" ]' > Dockerfile
 docker build -t alpine2 .
 
 
